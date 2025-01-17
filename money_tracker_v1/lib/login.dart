2a4ebb1e9029723475db2d92f563f182223a25ec
Nitheesh Saravanan <nitheesh5418@gmail.com>
2025-01-17 14:41:19 +0530

    change

--- money_tracker_v1/lib/login.dart
+++ money_tracker_v1/lib/login.dart
@@ -8,7 +8,8 @@ class LoginScreen extends StatefulWidget {
   _LoginScreenState createState() => _LoginScreenState();
 }
 
-class _LoginScreenState extends State<LoginScreen> with SingleTickerProviderStateMixin {
+class _LoginScreenState extends State<LoginScreen>
+    with SingleTickerProviderStateMixin {
   late TabController _tabController;
   String _selectedTab = 'Login';
 
@@ -30,36 +31,42 @@ class _LoginScreenState extends State<LoginScreen> with SingleTickerProviderStat
         return Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Email id',
-                filled: true,
-                labelStyle: TextStyle(
-                        fontSize: 20,
-                        color: const Color.fromARGB(152, 255, 255, 255),
-                        fontFamily: 'sans-serif Condensed',
-                        fontWeight: FontWeight.w300,
-                      ),
-                fillColor: Colors.transparent,
-                border: OutlineInputBorder(
-                  borderSide: BorderSide.none,
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: 'Email id',
+                  filled: true,
+                  labelStyle: TextStyle(
+                    fontSize: 20,
+                    color: const Color.fromARGB(152, 255, 255, 255),
+                    fontFamily: 'sans-serif Condensed',
+                    fontWeight: FontWeight.w300,
+                  ),
+                  fillColor: Colors.transparent,
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                  ),
                 ),
               ),
             ),
             SizedBox(height: 16),
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Password',
-                filled: true,
-                labelStyle: TextStyle(
-                        fontSize: 20,
-                        color: const Color.fromARGB(152, 255, 255, 255),
-                        fontFamily: 'sans-serif Condensed',
-                        fontWeight: FontWeight.w300,
-                      ),
-                fillColor: Colors.transparent,
-                border: OutlineInputBorder(
-                  borderSide: BorderSide.none,
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  filled: true,
+                  labelStyle: TextStyle(
+                    fontSize: 20,
+                    color: const Color.fromARGB(152, 255, 255, 255),
+                    fontFamily: 'sans-serif Condensed',
+                    fontWeight: FontWeight.w300,
+                  ),
+                  fillColor: Colors.transparent,
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                  ),
                 ),
               ),
             ),
@@ -67,31 +74,32 @@ class _LoginScreenState extends State<LoginScreen> with SingleTickerProviderStat
             ElevatedButton(
               onPressed: () {
                 Navigator.pushNamed(context, '/dashboard');
-              },style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color.fromARGB(255, 130, 37, 243),
-                      padding: EdgeInsets.symmetric(horizontal: 25, vertical: 20),
-                      animationDuration: Duration(seconds: 1),
-                      elevation: 4,
-                      shadowColor: const Color.fromARGB(255, 0, 0, 0),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(50),
-                      ),
-                      enableFeedback: true,
-                      
-                      textStyle: TextStyle(
-                        fontSize: 15,
-                        fontFamily: 'sans-serif Condensed',
-                        fontWeight: FontWeight.w300,
-                        shadows: [
-                          Shadow(
-                            color: const Color.fromARGB(152, 0, 0, 0),
-                            offset: Offset(3, 3),
-                            blurRadius: 6,
-                          ),
-                        ],
-                      ),
-                      foregroundColor: Colors.white, // Set the text color to white
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromARGB(255, 130, 37, 243),
+                padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+                animationDuration: Duration(seconds: 1),
+                elevation: 4,
+                shadowColor: const Color.fromARGB(255, 0, 0, 0),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(50),
+                ),
+                enableFeedback: true,
+
+                textStyle: TextStyle(
+                  fontSize: 15,
+                  fontFamily: 'sans-serif Condensed',
+                  fontWeight: FontWeight.w300,
+                  shadows: [
+                    Shadow(
+                      color: const Color.fromARGB(152, 0, 0, 0),
+                      offset: Offset(3, 3),
+                      blurRadius: 6,
                     ),
+                  ],
+                ),
+                foregroundColor: Colors.white, // Set the text color to white
+              ),
               child: Text("login"),
             ),
           ],
@@ -100,53 +108,62 @@ class _LoginScreenState extends State<LoginScreen> with SingleTickerProviderStat
         return Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Email id',
-                filled: true,
-                labelStyle: TextStyle(
-                        fontSize: 20,
-                        color: const Color.fromARGB(152, 255, 255, 255),
-                        fontFamily: 'sans-serif Condensed',
-                        fontWeight: FontWeight.w300,
-                      ),
-                fillColor: Colors.transparent,
-                border: OutlineInputBorder(
-                  borderSide: BorderSide.none,
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: 'Email id',
+                  filled: true,
+                  labelStyle: TextStyle(
+                    fontSize: 20,
+                    color: const Color.fromARGB(152, 255, 255, 255),
+                    fontFamily: 'sans-serif Condensed',
+                    fontWeight: FontWeight.w300,
+                  ),
+                  fillColor: Colors.transparent,
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                  ),
                 ),
               ),
             ),
             SizedBox(height: 16),
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Password',
-                filled: true,
-                labelStyle: TextStyle(
-                        fontSize: 20,
-                        color: const Color.fromARGB(152, 255, 255, 255),
-                        fontFamily: 'sans-serif Condensed',
-                        fontWeight: FontWeight.w300,
-                      ),
-                fillColor: Colors.transparent,
-                border: OutlineInputBorder(
-                  borderSide: BorderSide.none,
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  filled: true,
+                  labelStyle: TextStyle(
+                    fontSize: 20,
+                    color: const Color.fromARGB(152, 255, 255, 255),
+                    fontFamily: 'sans-serif Condensed',
+                    fontWeight: FontWeight.w300,
+                  ),
+                  fillColor: Colors.transparent,
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                  ),
                 ),
               ),
             ),
             SizedBox(height: 16),
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Re-Password',
-                filled: true,
-                labelStyle: TextStyle(
-                        fontSize: 20,
-                        color: const Color.fromARGB(152, 255, 255, 255),
-                        fontFamily: 'sans-serif Condensed',
-                        fontWeight: FontWeight.w300,
-                      ),
-                fillColor: Colors.transparent,
-                border: OutlineInputBorder(
-                  borderSide: BorderSide.none,
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: 'Re-Password',
+                  filled: true,
+                  labelStyle: TextStyle(
+                    fontSize: 20,
+                    color: const Color.fromARGB(152, 255, 255, 255),
+                    fontFamily: 'sans-serif Condensed',
+                    fontWeight: FontWeight.w300,
+                  ),
+                  fillColor: Colors.transparent,
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                  ),
                 ),
               ),
             ),
@@ -154,32 +171,32 @@ class _LoginScreenState extends State<LoginScreen> with SingleTickerProviderStat
             ElevatedButton(
               onPressed: () {
                 Navigator.pushNamed(context, '/dashboard');
-                
-              },style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color.fromARGB(255, 130, 37, 243),
-                      padding: EdgeInsets.symmetric(horizontal: 25, vertical: 20),
-                      animationDuration: Duration(seconds: 1),
-                      elevation: 4,
-                      shadowColor: const Color.fromARGB(255, 0, 0, 0),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(50),
-                      ),
-                      enableFeedback: true,
-                      
-                      textStyle: TextStyle(
-                        fontSize: 15,
-                        fontFamily: 'sans-serif Condensed',
-                        fontWeight: FontWeight.w300,
-                        shadows: [
-                          Shadow(
-                            color: const Color.fromARGB(152, 0, 0, 0),
-                            offset: Offset(3, 3),
-                            blurRadius: 6,
-                          ),
-                        ],
-                      ),
-                      foregroundColor: Colors.white, // Set the text color to white
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromARGB(255, 130, 37, 243),
+                padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+                animationDuration: Duration(seconds: 1),
+                elevation: 4,
+                shadowColor: const Color.fromARGB(255, 0, 0, 0),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(50),
+                ),
+                enableFeedback: true,
+
+                textStyle: TextStyle(
+                  fontSize: 15,
+                  fontFamily: 'sans-serif Condensed',
+                  fontWeight: FontWeight.w300,
+                  shadows: [
+                    Shadow(
+                      color: const Color.fromARGB(152, 0, 0, 0),
+                      offset: Offset(3, 3),
+                      blurRadius: 6,
                     ),
+                  ],
+                ),
+                foregroundColor: Colors.white, // Set the text color to white
+              ),
               child: Text("Signup"),
             ),
           ],
@@ -211,7 +228,6 @@ class _LoginScreenState extends State<LoginScreen> with SingleTickerProviderStat
             padding: const EdgeInsets.all(16.0),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            
               children: [
                 ElevatedButton(
                   onPressed: () {
@@ -221,32 +237,32 @@ class _LoginScreenState extends State<LoginScreen> with SingleTickerProviderStat
                     });
                   },
                   style: ElevatedButton.styleFrom(
-                    backgroundColor: _selectedTab == 'Login' ? 
-                    const Color.fromARGB(255, 255, 251, 4) : 
-                    const Color.fromARGB(255, 255, 255, 168),
-                    padding: EdgeInsets.symmetric(horizontal: 25, vertical: 20),
-                      elevation: 9,
-                      surfaceTintColor: const Color.fromARGB(255, 255, 207, 33),
-                      shadowColor: const Color.fromARGB(255, 255, 255, 0),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                      enableFeedback: true,
-                      
-                      textStyle: TextStyle(
-                        fontSize: _selectedTab == 'Login' ? 25 : 20,
-                        fontFamily: 'sans-serif Condensed',
-                        fontWeight: FontWeight.w500,
-                        shadows: [
-                          Shadow(
-                            color: const Color.fromARGB(49, 0, 0, 0),
-                            offset: Offset(1, 1),
-                            blurRadius: 6,
-                          ),
-                        ],
-                      ),
-                      foregroundColor: const Color.fromARGB(255, 51, 51, 51), // Set the text color to white
-                    
+                    backgroundColor: _selectedTab == 'Login'
+                        ? const Color.fromARGB(255, 255, 205, 4)
+                        : const Color.fromARGB(255, 253, 246, 218),
+                    padding: EdgeInsets.symmetric(horizontal: 25, vertical: 15),
+                    elevation: 9,
+                    surfaceTintColor: const Color.fromARGB(255, 255, 207, 33),
+                    shadowColor: const Color.fromARGB(255, 255, 255, 0),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    enableFeedback: true,
+
+                    textStyle: TextStyle(
+                      fontSize: _selectedTab == 'Login' ? 25 : 20,
+                      fontFamily: 'sans-serif Condensed',
+                      fontWeight: FontWeight.w500,
+                      shadows: [
+                        Shadow(
+                          color: const Color.fromARGB(49, 0, 0, 0),
+                          offset: Offset(1, 1),
+                          blurRadius: 6,
+                        ),
+                      ],
+                    ),
+                    foregroundColor: const Color.fromARGB(
+                        255, 51, 51, 51), // Set the text color to white
                   ),
                   child: Text('Login'),
                 ),
@@ -258,32 +274,32 @@ class _LoginScreenState extends State<LoginScreen> with SingleTickerProviderStat
                     });
                   },
                   style: ElevatedButton.styleFrom(
-                    backgroundColor: _selectedTab == 'Signup' ? 
-                    const Color.fromARGB(255, 255, 251, 4) : 
-                    const Color.fromARGB(255, 255, 255, 168),
-                    padding: EdgeInsets.symmetric(horizontal: 25, vertical: 20),
-                      elevation: 9,
-                      surfaceTintColor: const Color.fromARGB(255, 255, 207, 33),
-                      shadowColor: const Color.fromARGB(255, 255, 255, 0),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                      enableFeedback: true,
-                      
-                      textStyle: TextStyle(
-                        fontSize: _selectedTab == 'Signup' ? 25 : 20,
-                        fontFamily: 'sans-serif Condensed',
-                        fontWeight: FontWeight.w500,
-                        shadows: [
-                          Shadow(
-                            color: const Color.fromARGB(49, 0, 0, 0),
-                            offset: Offset(1, 1),
-                            blurRadius: 6,
-                          ),
-                        ],
-                      ),
-                      foregroundColor: const Color.fromARGB(255, 51, 51, 51), // Set the text color to white
-                    
+                    backgroundColor: _selectedTab == 'Signup'
+                        ? const Color.fromARGB(255, 255, 205, 4)
+                        : const Color.fromARGB(255, 253, 246, 218),
+                    padding: EdgeInsets.symmetric(horizontal: 25, vertical: 15),
+                    elevation: 9,
+                    surfaceTintColor: const Color.fromARGB(255, 255, 207, 33),
+                    shadowColor: const Color.fromARGB(255, 255, 255, 0),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    enableFeedback: true,
+
+                    textStyle: TextStyle(
+                      fontSize: _selectedTab == 'Signup' ? 25 : 20,
+                      fontFamily: 'sans-serif Condensed',
+                      fontWeight: FontWeight.w500,
+                      shadows: [
+                        Shadow(
+                          color: const Color.fromARGB(49, 0, 0, 0),
+                          offset: Offset(1, 1),
+                          blurRadius: 6,
+                        ),
+                      ],
+                    ),
+                    foregroundColor: const Color.fromARGB(
+                        255, 51, 51, 51), // Set the text color to white
                   ),
                   child: Text('Signup'),
                 ),
